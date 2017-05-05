8529be6d7b3e83100944a6625a4eaa30b6a3bb2b
Jay Cribas <feverjay@gmail.com>
2017-05-05 14:26:41 -0700

    Switch to localhost 3000, update readme

--- passport.js
+++ passport.js
@@ -11,7 +11,7 @@ const User = {
 const strategy = new TwitterStrategy({
   consumerKey: process.env.API_KEY,
   consumerSecret: process.env.API_SECRET,
-  callbackURL: 'http://localhost:9000/auth/twitter/callback'
+  callbackURL: 'http://localhost:3000/auth/twitter/callback'
 },
 function(token, tokenSecret, profile, done) {
   User.findOrCreate({ displayname: 'JPH5_' }, function(err, user){
